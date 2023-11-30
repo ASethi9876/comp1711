1a1bd2eafbf1392e8f8ea6b2a7a9ad37f935d63b
ASethi9876 <146444827+ASethi9876@users.noreply.github.com>
2023-11-30 16:49:54 +0000

    cw

--- coursework/task3/FitnessDataSorter.c
+++ coursework/task3/FitnessDataSorter.c
@@ -27,12 +27,44 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
 
 
 int main() {
-    char* filename = "";
+    char filename[100];
     printf("Enter filename: ");
     scanf("%s",filename);
+
+    
     FILE *file = fopen(filename,"r");
     if (file == NULL) {
         printf("Error: invalid file\n");
         return 1;
     }
+
+    char* nameAppend = ".tsv";
+    strncat(filename,nameAppend,4);
+    
+    FitnessData data[10000];
+    int counter = 0;
+    int buffer_size = 500;
+    char line[buffer_size];
+    char date[11];
+    char time[6];
+    char delim;
+    int* steps;
+
+    while (fgets(line, buffer_size, file)){
+        tokeniseRecord(line, delim, date, time, steps);
+        counter++;
+    }
+
+    fclose(file);
+    char* nameAppend = ".tsv";
+    strncat(filename,nameAppend,4);
+
+    FILE *newfile = fopen(filename, "w"); // or "a", "w+", "a+"
+    if (newfile == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+    fclose(newfile);
+    return 0;
 }
